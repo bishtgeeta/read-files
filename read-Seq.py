5f1e76c1ca1280e48c7ab76ccd7813a0c131a819
Praveen <deorani@gmail.com>
2017-01-06 18:12:07 +0800

    corrected path for saving output files in seq-reader

--- read-Seq.py
+++ read-Seq.py
@@ -16,8 +16,16 @@ for subpath, dirs, files in list(os.walk(path)):
     if len(files) > 0:
         for fname in files:
             input_fname = join(subpath, fname)
-            output_fname = join(output_subpath, fname)
+            ## make a folder corresponding to every input seq file
+            this_foldername = join(output_subpath, fname.replace('.seq', '')
+            if not exists(this_foldername):
+                os.mkdir(this_foldername)
             vid = pims.open(input_fname)
-            for img in vid:
-                _img = ((img - img.min()) * 1.0 /  (img.max() - img.min()) * 255).astype(np.uint8)
-                cv2.imwrite(join(output_fname, img).png, _img)
+            ## need to find how many frames are there in vid
+            ## when you print dir(vid), you can see an attribute called `_image_count`
+            num_frames = vid._image_count
+            for n in range(num_frames):
+                img = vid.get_frame(n)
+                img = ((img - img.min()) * 1.0 /  (img.max() - img.min()) * 255).astype(np.uint8)
+                to_save_path = join(this_foldername, '{0}.png'.format(n))
+                cv2.imwrite(to_save_path, img)
